697e2ac0cdaee867815294a9f28e501186049b2c
Praveen869 <praveenkrdwivedi.3249@gmail.com>
2026-05-28 14:13:22 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://carriercraft-ai.vercel.app/"
+    "https://carriercraft-ai.vercel.app"
   ],
 
   credentials: true,
